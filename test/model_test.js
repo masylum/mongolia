e2639ce53b5269c89f508f52a06c1f97835894c9
Josep M. Bach <josep.m.bach@gmail.com>
2011-04-06 21:30:11 +0200

    Failing tests

--- test/model_test.js
+++ test/model_test.js
@@ -1,28 +1,87 @@
-var testosterone = require('testosterone')({post: 3000, sync: true}),
-    assert = testosterone.assert;
+var testosterone = require('testosterone')({post: 3000}),
+    assert = testosterone.assert,
 
-var Model = require('./../lib/model'),
+    Model = require('./../lib/model'),
     mongodb = require('mongodb'),
     Db = mongodb.Db,
     Server = mongodb.Server,
-    db = new Db('mongolia_test', new Server('localhost', 27017, {auto_reconnect: true, native_parser: true}), {});
+
+    db = new Db('mongolia_test', new Server('localhost', 27017, {auto_reconnect: true, native_parser: true}), {}),
+
+    User = function (db) {
+      var user = Model(db, 'users');
+      return user;
+    },
+
+    User = User(db),
+    sys = require('sys'),
+
+    load_user = function (callback) {
+      db.collection('users', function (error, collection) {
+        collection.insert({name: 'Pau', email: 'pau@mongolia.com', password: 'pau123'}, function (error, doc) {
+          callback(doc);
+          collection.remove({}, function (error, bla) {});
+        });
+      });
+    },
+
+    remove_users = function () {
+      db.collection('users', function (error, collection) {
+        collection.remove({}, function (error, bla) {});
+      });
+    };
+
+db.open();
 
 testosterone
 
-  .add('WHEN there is no db\n' +
-       'THEN it throws an error', function (spec) {
+  .add('Throws an error when there is no db', function (done) {
     assert.throws(function () {
-        var model = new Model(null);
-    }, /You must specify a db/);
+        var model = Model(null);
+      }, /You must specify a db/);
   })
 
-  .add('WHEN there is no collection\n' +
-       'THEN it throws an error', function (spec) {
+  .add('Throws an error when collection is missing', function (done) {
     assert.throws(function () {
-        var model = new Model(db);
-    }, /You must specify a collection name/);
+        var model = Model(db);
+      }, /You must specify a collection name/);
   })
+
+  .add('#getCollection returns a document collection', function (done) {
+    User.getCollection(function (error, collection) {
+      assert.equal(error, null);
+      assert.equal(collection.collectionName, 'users');
+    });
+  })
+
+  .add('#call findArray', function (done) {
+    load_user(function (user) {
+      var n = 0;
+
+      // User.mongoCall('findArray', {}, function (error, doc) {
+      //   console.log(doc);
+      //   assert.equal(doc, user);
+      //   n += 1;
+      // });
+
+      User.mongoCall('findArray', {name: 'Pau'}, function (error, doc) {
+        assert.equal(doc, user);
+        n += 1;
+      });
+
+      // User.mongoCall('findArray', {name: 'Zemba'}, function (error, doc) {
+      //   assert.equal(doc, []);
+      //   n += 1;
+      // });
+
+      done(function () {
+        assert.equal(n, 3);
+      });
+    });
+  })
+
 
   .run(function () {
     require('sys').print('done!');
+    db.close();
   });
