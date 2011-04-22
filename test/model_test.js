5f9c7fac1a82da75de82cadd844dd5481181018b
Josep M. Bach <josep.m.bach@gmail.com>
2011-04-22 17:54:37 +0200

    beforeCreate and afterCreate always handle arrays of documents

--- test/model_test.js
+++ test/model_test.js
@@ -124,18 +124,21 @@ testosterone
   })
 
   .add('`beforeCreate` default hook sets the created_at date and runs the callback', function () {
-    var document = {};
+    var documents = [{name: 'zemba', foo: 'bar'}];
 
-    User.beforeCreate(document, gently.expect(function () {
+    User.beforeCreate(documents, gently.expect(function () {
       // Ensure #created_at is a Date
-      assert.ok(document.created_at, 'Model#beforeCreate should set document#created_at to be a Date');
-      assert.equal(document.created_at.constructor, (new Date()).constructor);
+      documents.forEach(function (document) {
+        assert.ok(document.created_at, 'Model#beforeCreate should set document#created_at to be a Date');
+        assert.equal(document.created_at.constructor, (new Date()).constructor);
+      });
     }));
   })
 
   .add('`afterCreate` default hook just runs the callback', function () {
-    var document = {};
-    User.afterCreate(document, gently.expect(function () {}));
+    var documents = [{name: 'zemba', foo: 'bar'}];
+
+    User.afterCreate(documents, gently.expect(function () {}));
   })
 
   .add('`beforeUpdate` default hook updated the updated_at date and runs the callback', function () {
